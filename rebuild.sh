a76b2e4b0c8451b42008c3d5ba682caaf7904bcc
Jon Hermansen <jon@jh86.org>
2025-10-20 23:19:09 +0000

    simplify rebuild now that flakes is enabled

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-nix --extra-experimental-features "flakes nix-command" build .#
+nix build .#
 ./result/activate
 initctl reload
-nix --extra-experimental-features "flakes nix-command" build --profile /nix/var/nix/profiles/system .#
-nix --extra-experimental-features "flakes nix-command" run .#grub
+
+nix build --profile /nix/var/nix/profiles/system .#
+nix run .#grub
